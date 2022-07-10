e7c119f8eb2dd656fff69496680643bb1501c55a
Deelaka Lakpura <deelaka.lakpura94@gmail.com>
2022-07-10 14:57:44 +0530

    first commit

--- addnewbook.php
+++ addnewbook.php
@@ -202,16 +202,7 @@
 </section>
 		
         </div>
-        <!--**********************************
-            Content body end
-        ***********************************-->
-		
-
-
-		
-        <!--**********************************
-            Footer start
-        ***********************************-->
+       
         <div class="footer">
             <div class="copyright">
                 <p>Copyright © Designed &amp; Developed by <a href="../index.htm" target="_blank">DexignLab </a>2022</p>
